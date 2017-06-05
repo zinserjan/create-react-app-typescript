eeafa9b16151124066f2b02d76ca43ca1bf3f1d0
Jan-André Zinser <zinserjan@gmail.com>
2017-06-05 20:23:54 +0200

    Fix code style for template

--- packages/react-scripts/template/src/registerServiceWorker.ts
+++ packages/react-scripts/template/src/registerServiceWorker.ts
@@ -9,8 +9,8 @@
 // This link also includes instructions on opting out of this behavior.
 
 export default function register() {
-  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
+  if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
       navigator.serviceWorker
         .register(swUrl)
@@ -18,11 +18,11 @@ export default function register() {
           registration.onupdatefound = () => {
             const installingWorker = registration.installing;
             if (!installingWorker) {
-                return;
+              return;
             }
 
             installingWorker.onstatechange = () => {
-              if (installingWorker.state === 'installed') {
+              if (installingWorker.state === "installed") {
                 if (navigator.serviceWorker.controller) {
                   // At this point, the old content will have been purged and
                   // the fresh content will have been added to the cache.
@@ -47,7 +47,7 @@ export default function register() {
 }
 
 export function unregister() {
-  if ('serviceWorker' in navigator) {
+  if ("serviceWorker" in navigator) {
     navigator.serviceWorker.ready.then(registration => {
       registration.unregister();
     });
